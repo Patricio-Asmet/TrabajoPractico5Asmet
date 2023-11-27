e444899c4cce2c430e3e92b628d727ec61d65426
Patricio Asmet <patricioasmet@gmail.com>
2023-11-27 19:04:26 -0300

    ejercicio nro1

---
+++ js/ejercicio1.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    var numeroMagico;
+
+    $("#comenzarBtn").click(function () {
+      numeroMagico = Math.floor(Math.random() * 100) + 1;
+      
+      $("#comenzarBtn").hide();
+      $("#juego").show();
+    });
+
+    $("#enviarBtn").click(function () {
+      var numeroUsuario = $("#numeroInput").val();
+      
+      if (numeroUsuario == numeroMagico) {
+        alert("¡Felicidades! Adivinaste el número.");
+        reiniciarJuego();
+      } else if (numeroUsuario < numeroMagico) {
+        alert("Intenta con un número mayor.");
+      } else {
+        alert("Intenta con un número menor.");
+      }
+    });
+
+    function reiniciarJuego() {
+      $("#comenzarBtn").show();
+      $("#juego").hide();
+      $("#numeroInput").val("");
+    }
+  });
